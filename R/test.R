3be594f8b403720df3b499b5bf97b897c8d2cca4
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-01-28 14:32:07 -0500

    Implement `test()`

--- R/test.R
+++ R/test.R
@@ -1,1 +1,25 @@
 # TODO: Define the user-facing `test()` function and class.
+
+test <- function(
+    .expr,
+    ...,
+    .env = rlang::caller_env()
+  ) {
+  check_is_env <- identity # TODO
+
+  fmls <- rlang::pairlist2(x = , !!!rlang::exprs(...))
+  body <- rlang::enexpr(.expr)
+  env <- check_is_env(.env)
+
+  out <- rlang::new_function(
+    args = fmls,
+    body = body,
+    env = env
+  )
+  class(out) <- c("checkwriter_test", "function")
+  out
+}
+
+is_test <- function(x) {
+  inherits(x, "checkwriter_test")
+}
